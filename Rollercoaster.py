0b5cb453e4f0dc678f0ab9fed593c61cea380727
ClaudioHplus <mezza.claudio@gmail.com>
2024-07-04 12:23:54 +0200

    Aggiunta la classe 'Umano'

--- Rollercoaster.py
+++ Rollercoaster.py
@@ -4,8 +4,22 @@ class PuntoCartesiano:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
+    def __str__(self):
+        return f"({self.x}, {self.y})" 
 
 # Definire la classe Umano
+
+class Umano:
+    def __init__(self, nome: str, cognome: str, posizione: PuntoCartesiano, tipo: str, attrazioniDesiderate: list[str]):
+        self.nome = nome
+        self.cognome = cognome
+        self.posizione = posizione
+        self.tipo = tipo
+        self.attrazioniDesiderate = attrazioniDesiderate
+    
+    def __str__(self):
+        return f'{self.nome}, {self.cognome}, {self.posizione}, {self.tipo}, {self.attrazioniDesiderate}'
+
 
 # Definire la classe Bambino (ereditaria Umano)
 

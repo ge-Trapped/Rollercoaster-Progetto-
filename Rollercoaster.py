39c79731ae46c7daf5889787585f3729ca3d7621
ge-Trapped <santabarbara.sergio@gmail.com>
2024-07-04 11:03:47 +0200

    Update Rollercoaster.py
    Commenti "Definizione classi"

---
+++ Rollercoaster.py
@@ -0,0 +1,16 @@
+# DEFINIRE TUTTE LE CLASSI:
+# Definire la classe PuntoCartesiano
+
+# Definire la classe Umano
+
+# Definire la classe Bambino (ereditaria Umano)
+
+# Definire la classe Ragazzo (ereditaria Umano)
+
+# Definire la classe Adulto (ereditaria Umano)
+
+# Definire la classe Location
+
+# Definire la classe Ristoro
+
+# Definire la classe Attrazione

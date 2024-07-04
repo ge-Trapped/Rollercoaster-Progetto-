99c7db2d270c15e0487299297a0968fb9bf4cfb6
ge-Trapped <santabarbara.sergio@gmail.com>
2024-07-04 13:00:59 +0200

    Update Rollercoaster.py
    Aggiunta la classe "Location"

--- Rollercoaster.py
+++ Rollercoaster.py
@@ -1,4 +1,5 @@
-# DEFINIRE TUTTE LE CLASSI:
+## DEFINIRE TUTTE LE CLASSI:
+
 # Definire la classe PuntoCartesiano
 class PuntoCartesiano:
     def __init__(self, x: int, y: int):
@@ -22,25 +23,31 @@ class Umano:
     def __str__(self):
         return f'{self.nome}, {self.cognome}, {self.posizione}, {self.tipo}, {self.attrazioniDesiderate}'
 
-
 # Definire la classe Bambino (ereditaria Umano)
 class Bambino(Umano):
     def __init__(self, nome: str, cognome: str, posizione: PuntoCartesiano):
         super().__init__(nome, cognome, posizione, "Bambino", ["tazze", "bruco", "covo dei pirati"])
 
 # Definire la classe Ragazzo (ereditaria Umano)
-
 class Ragazzo(Umano):
     def __init__(self, nome: str, cognome: str, posizione: PuntoCartesiano):
         super().__init__(nome, cognome, posizione, "Ragazzo", ["Raptor", "Blue Tornado", "Space Vertigo"])
 
 # Definire la classe Adulto (ereditaria Umano)
-
 class Adulto(Umano):
     def __init__(self, nome: str, cognome: str, posizione: PuntoCartesiano):
         super().__init__(nome, cognome, posizione, "Adulto", [""])
 
 # Definire la classe Location
+class Location():
+    def __init__(self, nome: str, posizione: PuntoCartesiano):
+        self.nome = nome
+        self.posizione = posizione
+
+    def __str__(self):
+        return f"{self.nome}, {self.posizione}"
+
+
 
 # Definire la classe Ristoro
 

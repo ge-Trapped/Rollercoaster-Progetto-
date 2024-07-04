745e7fa44dad72681816df220f9a3c916e4f038b
ge-Trapped <santabarbara.sergio@gmail.com>
2024-07-04 12:37:45 +0200

    Update Rollercoaster.py
    Aggiunta la sottoclasse "Bambino"

--- Rollercoaster.py
+++ Rollercoaster.py
@@ -4,11 +4,11 @@ class PuntoCartesiano:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
+    
     def __str__(self):
         return f"({self.x}, {self.y})" 
 
 # Definire la classe Umano
-
 class Umano:
     def __init__(self, nome: str, cognome: str, posizione: PuntoCartesiano, tipo: str, attrazioniDesiderate: list[str]):
         self.nome = nome
@@ -22,6 +22,13 @@ class Umano:
 
 
 # Definire la classe Bambino (ereditaria Umano)
+class Bambino(Umano):
+    def __init__(self, nome: str, cognome: str, posizione: PuntoCartesiano):
+        super().__init__(nome, cognome, posizione, "Bambino", ["tazze", "bruco", "covo dei pirati"])
+
+    def __str__(self):
+        return f'{self.nome}, {self.cognome}, {self.posizione}, {self.tipo}, {self.attrazioniDesiderate}'
+
 
 # Definire la classe Ragazzo (ereditaria Umano)
 
